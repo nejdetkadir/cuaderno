a1d46f6f222ab8a121d6a0d7c6a62aebba0f6205
nejdetkadir <nejdetkadir.550@gmail.com>
2020-12-31 06:21:43 +0300

    Import bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-6">
+        left
+      </div>
+      <div class="col-md-6">
+        right
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .col-md-6{
+    background-color: #42b983;
+  }
 </style>
